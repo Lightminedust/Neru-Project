caf4d2e501118775ff52c6890dcaa561c83e9181
Nell <nellawassi@gmail.com>
2023-03-12 19:20:13 +0100

    V3

--- data/dataForm.php
+++ data/dataForm.php
@@ -1,14 +1,30 @@
 <?php
-$data = json_decode(file_get_contents('php://input'), true);
-$file = 'formdata.json';
-$current = file_get_contents($file);
-$current .= json_encode($data) . "\n";
-file_put_contents($file, $current);
-?>
-<?php
-$data = json_decode(file_get_contents('php://input'), true);
-$file = 'data.json';
-$current = file_get_contents($file);
-$current .= json_encode($data) . "\n";
-file_put_contents($file, $current);
+// Ouvre le fichier JSON
+$file = '../data/data.json';
+$current_data = file_get_contents($file);
+
+// Décodage du contenu JSON en tableau associatif
+$array_data = json_decode($current_data, true);
+
+// Ajout des données POST envoyées par le formulaire au tableau associatif
+$form_data = array(
+    'Name' => $_POST['Name'],
+    'Prenom' => $_POST['Prenom'],
+    'Age' => $_POST['Age'],
+    'Mail' => $_POST['Mail'],
+    'Password' => $_POST['Password']
+);
+$array_data[] = $form_data;
+
+// Réencodage du tableau associatif en JSON
+$json_data = json_encode($array_data, JSON_PRETTY_PRINT);
+
+// Écriture du contenu JSON dans le fichier
+file_put_contents($file, $json_data);
+
+// Affichage des données dans la console
+var_dump($form_data);
+
+// Réponse HTTP
+http_response_code(200);
 ?>
