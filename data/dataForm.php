67f7399e632a0bf071621a8c859b478b7a32d3d8
Nell <116392257+Lightminedust@users.noreply.github.com>
2023-03-12 18:58:04 +0100

    Update dataForm.php

--- data/dataForm.php
+++ data/dataForm.php
@@ -1,10 +1,4 @@
-<?php
-$data = json_decode(file_get_contents('php://input'), true);
-$file = 'formdata.json';
-$current = file_get_contents($file);
-$current .= json_encode($data) . "\n";
-file_put_contents($file, $current);
-?>
+
 <?php
 $data = json_decode(file_get_contents('php://input'), true);
 $file = 'data.json';
