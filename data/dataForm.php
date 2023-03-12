f7c9b01c2694081de3824a5d329ce4d7d9bfb694
Nell <nellawassi@gmail.com>
2023-03-12 18:49:10 +0100

    V.

--- data/dataForm.php
+++ data/dataForm.php
@@ -1,34 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["Name"];
-  $prenom = $_POST["Prenom"];
-  $age = $_POST["Age"];
-  $mail = $_POST["Mail"];
-  $password = $_POST["Password"];
-
-  // Créer un objet JSON avec les valeurs des champs de saisie
-  $formData = array(
-    "Nom" => $name,
-    "Prénom" => $prenom,
-    "Age" => $age,
-    "Email" => $mail,
-    "Mot de passe" => $password
-  );
-
-  // Écrire les données JSON dans un fichier
-  $file_path = "../data/data.json";
-  $file = fopen($file_path, 'w');
-  fwrite($file, json_encode($formData));
-  fclose($file);
-
-  // Répondre avec un message de confirmation
-  $response = array('message' => 'Les données ont été enregistrées avec succès !');
-  echo json_encode($response);
-}
+$data = json_decode(file_get_contents('php://input'), true);
+$file = 'formdata.json';
+$current = file_get_contents($file);
+$current .= json_encode($data) . "\n";
+file_put_contents($file, $current);
+?>
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$file = 'data.json';
+$current = file_get_contents($file);
+$current .= json_encode($data) . "\n";
+file_put_contents($file, $current);
 ?>
