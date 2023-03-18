31f8139571d0b39642f5db23cb8d2f5a56850bfe
Nell <nellawassi@gmail.com>
2023-03-18 18:46:45 +0100

    V7

--- Js/authFireBase.js
+++ Js/authFireBase.js
@@ -1,7 +1,11 @@
-const apiKeyOpenAi = "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";
+const apiKeyOpenAi = "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";
 const chatForm = document.querySelector("#entry form");
-const dialogBox = document.querySelector("#dialogBox");
-const historique = document.querySelector("#historique");
+const dialogBox = document.getElementById('chatMess');
+
+// Fonction pour faire défiler la boîte de chat vers le bas
+function scrollToBottom() {
+  dialogBox.scrollTop = dialogBox.scrollHeight;
+}
 
 chatForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,18 +15,14 @@ chatForm.addEventListener("submit", (event) => {
   }
 
   // Ajouter le message de l'utilisateur au dialogue
-
   const userMessage = document.createElement("div");
   userMessage.innerHTML = userInput.replace(/\n/g, "<br>");
   userMessage.classList.add("user-message");
+  userMessage.style.position = 'relative';
+  userMessage.style.right = '0'
+  userMessage.style.marginLeft = "60vh"
+  userMessage.style.width = "80%";
   dialogBox.appendChild(userMessage);
-  userMessage.style.width = "100%";
-  userMessage.style.backgroundColor = "#9eabb4";
-  userMessage.style.borderRadius = "15px";
-  userMessage.style.paddingLeft = "10px";
-
-  // Faire défiler la boîte de dialogue jusqu'au bas pour afficher le nouveau message
-  dialogBox.scrollTop = dialogBox.scrollHeight;
 
   // Envoyer la requête à l'API OpenAI
   const requestOptions = {
@@ -32,14 +32,14 @@ chatForm.addEventListener("submit", (event) => {
       Authorization: "Bearer " + apiKeyOpenAi,
     },
     body: JSON.stringify({
-      model: "text-davinci-002",
+      model: "text-davinci-003",
       prompt:
         "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: " +
         userInput +
         "\nAI:",
-      temperature: 0.9,
+      temperature: 0.2,
       max_tokens: 4000,
-      top_p: 1,
+      top_p: 0.5,
       frequency_penalty: 0,
       presence_penalty: 0.6,
       stop: [" Human:", " AI:"],
@@ -49,20 +49,20 @@ chatForm.addEventListener("submit", (event) => {
   fetch("https://api.openai.com/v1/completions", requestOptions)
     .then((response) => response.json())
     .then((data) => {
-      const aiMessage = document.createElement("p");
+      const aiMessage = document.createElement("div");
       aiMessage.innerHTML = data.choices[0].text.replace(/\n/g, "<br>");
       aiMessage.classList.add("ai-message");
-      dialogBox.appendChild(aiMessage);
-      aiMessage.style.width = "95%";
-      aiMessage.style.overflowY = 'scroll'
+      aiMessage.style.width = "50%";
       aiMessage.style.backgroundColor = "#ffffff";
       aiMessage.style.borderRadius = "15px";
-      aiMessage.style.paddingLeft = "10px";
-      historique.appendChild(dialogBox);
-      dialogBox.scrollTop = dialogBox.scrollHeight;
+      aiMessage.style.marginLeft = "60vh"
+      aiMessage.style.padding = '10px'
+      aiMessage.style.textAlign = 'justify';
+      dialogBox.appendChild(aiMessage);
+      // Faire défiler la boîte de chat vers le bas après chaque nouveau message ajouté
+      scrollToBottom();
     })
     .catch((error) => console.log(error));
-
 
   // Effacer l'entrée utilisateur
   document.querySelector("#entryField").value = "";
